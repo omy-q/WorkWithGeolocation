be65ea0edad5ad3b4edc0353447316aae6ed3ffd
omy-q <olya_aparina@mail.ru>
2021-08-29 18:40:40 +0300

    Изменение макета активити и использование binding

--- app/src/main/java/com/example/workwithgeolocation/MainActivity.kt
+++ app/src/main/java/com/example/workwithgeolocation/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.workwithgeolocation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.workwithgeolocation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        savedInstanceState.let { supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragment_container, GeolocationFragment.newInstance())
+            .commit()
+        }
     }
 }
