70cc809717a56c3024ce6d0a1c87cde05ad35135
omy-q <olya_aparina@mail.ru>
2021-08-29 22:07:24 +0300

    Создание фрагмента для работы с google картами

---
+++ app/src/main/java/com/example/workwithgeolocation/GoogleMapsFragment.kt
@@ -0,0 +1,119 @@
+package com.example.workwithgeolocation
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.location.Address
+import android.location.Geocoder
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.workwithgeolocation.databinding.FragmentMapsBinding
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import java.io.IOException
+
+class MapsFragment : Fragment() {
+
+    private lateinit var map: GoogleMap
+    private val markers: ArrayList<Marker> = arrayListOf()
+    private var _binding: FragmentMapsBinding? = null
+    private val binding get() = _binding!!
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        map = googleMap
+        val initialPlace = LatLng(59.0155556, 30.005)
+        googleMap.addMarker(
+            MarkerOptions().position(initialPlace).title(getString(R.string.marker_start))
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(initialPlace))
+        myLocation(googleMap)
+        googleMap.uiSettings.isZoomControlsEnabled = true
+    }
+
+    private fun myLocation(googleMap: GoogleMap) {
+        context?.let {
+            val isPermissionGranted =
+                ContextCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED
+            googleMap.isMyLocationEnabled = isPermissionGranted
+            googleMap.uiSettings.isMyLocationButtonEnabled = isPermissionGranted
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMapsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+        initSearchByAddress()
+    }
+
+    private fun initSearchByAddress() {
+        binding.buttonSearch.setOnClickListener {
+            val geoCoder = Geocoder(it.context)
+            val searchText = binding.searchAddress.text.toString()
+            Thread {
+                try {
+                    val addresses = geoCoder.getFromLocationName(searchText, 1)
+                    if (addresses.size > 0) {
+                        goToAddress(addresses, it, searchText)
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }.start()
+        }
+    }
+
+    private fun goToAddress(
+        addresses: MutableList<Address>,
+        view: View,
+        searchText: String
+    ) {
+        val location = LatLng(
+            addresses[0].latitude,
+            addresses[0].longitude)
+        view.post {
+            setMarker(location, searchText)
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 25f))
+        }
+    }
+
+    private fun setMarker(location: LatLng, searchText: String): Marker {
+        return map.addMarker(MarkerOptions()
+                .position(location)
+                .title(searchText))
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
